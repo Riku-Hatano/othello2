a6ce8ccb820d480f81e8ad889914ee5768a32788
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2024-03-17 23:52:34 +0900

    add: implemented post request for backend

--- components/TestButton.tsx
+++ components/TestButton.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { store } from "@/redux/store";
 
 const testConnect = async () => {
-  const data = axios.create().get("http://localhost:3030");
+  const score = store.getState().board.scores;
+  const data = axios.create().post("http://localhost:3030", score);
   const result = await data;
   console.log(result);
 };
